286018d91f963dbcdb40a48e16ad4972e76fc1d9
iamSleepl3ss <aleksimm@hotmail.com>
2016-10-26 07:32:17 +0300

    Lagt til ferdig utviklet Javascript elementer.
    Inkludert kode for uhell timer...
     - Vadim

---
+++ DagboksSideFerdig/js/transitions.js
@@ -0,0 +1,52 @@
+function toInfo() {
+	document.getElementById("logo").style.display = "none";
+	document.getElementById("info").style.display = "block";
+	document.getElementById("projects").style.display = "none";
+	document.getElementById("bgoff").style.opacity = "0";
+	document.getElementById("bgoff2").style.opacity = "1";
+	document.getElementById("body1").style.opacity = "0";
+	document.getElementById("body2").style.opacity = "1";
+}
+
+function toProjects() {
+	document.getElementById("logo").style.display = "none";
+	document.getElementById("info").style.display = "none";
+	document.getElementById("projects").style.display = "block";
+	document.getElementById("bgoff").style.opacity = "1";
+	document.getElementById("bgoff2").style.opacity = "0";
+	document.getElementById("body1").style.opacity = "0";
+	document.getElementById("body2").style.opacity = "1";
+}
+
+function toHome() {
+	document.getElementById("logo").style.display = "block";
+	document.getElementById("info").style.display = "none";
+	document.getElementById("projects").style.display = "none";
+	document.getElementById("bgoff").style.opacity = "0";
+	document.getElementById("bgoff2").style.opacity = "0";
+	document.getElementById("body1").style.opacity = "0";
+	document.getElementById("body2").style.opacity = "1";
+}
+
+window.onload = countTimer;
+
+var timerVar = setInterval(countTimer, 1000);
+var totalSeconds = 0;
+var ulykke = Math.floor((Math.random() * 9) + 3)
+		console.log(ulykke)
+
+		function countTimer() {
+		++totalSeconds;
+		var hour = Math.floor(totalSeconds / 3600);
+		var minute = Math.floor((totalSeconds - hour * 3600) / 60);
+		var seconds = totalSeconds - (hour * 3600 + minute * 60);
+
+		if(seconds === ulykke){
+			totalSeconds=0
+		}
+		 document.getElementById("print").innerHTML = hour + ":" + minute + ":" + seconds;
+		 if(document.getElementById("print").innerHTML == "0:0:1"){
+			 document.getElementById("print").innerHTML = "<span class=\"red\">0:0:1</span>"
+		 }
+
+}
